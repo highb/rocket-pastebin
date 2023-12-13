98b7ad13d6a6d0425d0019001c3263e2ee7abec3
Brandon <highb@users.noreply.github.com>
2023-12-12 21:45:12 -0800

    Setup GET / and paste_id impl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[macro_use] extern crate rocket;
 
+mod paste_id;
+
+// use paste_id::PasteId;
+use std::path::Path;
+use rocket::tokio::fs::File;
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, retrieve])
 }
 
 #[get("/")]
@@ -19,4 +26,11 @@ fn index() -> &'static str {
 
           retrieves the content for the paste with id `<id>`
     "
+}
+
+#[get("/<id>")]
+async fn retrieve(id: &str) -> Option<File> {
+    let upload_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/", "upload");
+    let filename = Path::new(upload_dir).join(id);
+    File::open(&filename).await.ok()
 }
